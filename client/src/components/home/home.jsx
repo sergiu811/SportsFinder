03eab54f5a3b891e4c345ad369de4ede71bdd9cc
Sergiu Idu <idu_sergiu@yahoo.com>
2023-05-14 17:13:53 +0300

    Updates in the UI

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,17 +1,32 @@
-import { GridItem, Grid } from "@chakra-ui/react";
+import {
+  GridItem,
+  Grid,
+  Box,
+  VStack,
+  Heading,
+  AbsoluteCenter,
+} from "@chakra-ui/react";
 import CourtList from "../court-list/court-list";
 import ProfileCard from "../profile/profile-card";
+import { Typography } from "antd";
+
+const { Title } = Typography;
 
 const Home = () => {
   return (
-    <Grid templateColumns={"4fr 6fr"}>
-      <GridItem>
-        <ProfileCard></ProfileCard>
-      </GridItem>
-      <GridItem>
-        <CourtList></CourtList>
-      </GridItem>
-    </Grid>
+    <Box
+      width={"100%"}
+      marginTop="100px"
+      position="relative"
+      display="flex"
+      margin={"auto"}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <VStack>
+        s <CourtList></CourtList>
+      </VStack>
+    </Box>
   );
 };
 
