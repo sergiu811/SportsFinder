9585f709c7d6310e3af47ea383eab5bf68fcbfd5
Sergiu Idu <idu_sergiu@yahoo.com>
2023-05-26 14:21:00 +0300

    Updated

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = (user) =>
-  io("http://localhost:5001", {
+  io("http://164.90.164.219:5001", {
     transports: ["websocket"],
     autoConnect: false,
     withCredentials: true,
